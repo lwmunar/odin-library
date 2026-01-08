2663d5260fcd0cae42cb5296ba249a0ad6d32751
lwmunar <lmunar.gg@gmail.com>
2026-01-09 02:09:06 +0800

    Finish a working library with add book function

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 TOP's Project: Library
 
 Create a library that stores books
+
+
+Notes:
+- when using a callback function in an element.addEventListener, 'this' will refer to the 'element' or the 'event.target'. However, using arrow functions will refer 'this' to the global window context.
+
+- for text/numeric inputs their content will come from 'element.value', while for checkboxes, their on/off state should be checked using 'element.checked'.
+
+- to reset the form to a blank state, use .reset():
+
+let form = document.querySelector('form');
+...
+form.reset();
--- script.js
+++ script.js
@@ -1,65 +1,181 @@
-// Create array for storage
-// Create book constructor - title, author, pages, unique ID, read?
-// Test new book with dummy values and display in dummy DOM
-// Create function to store book data - will be linked to form submit button
-// Create form area in HTML - will look like a library card
-// Display books in page shelf
-// Add buttons on each book to remove from shelf and for toggling read status
-
 let bookLibrary = [];
 
-function NewBook(title, author, pages, id, read) {
+function Book(title, author, pages, read, id) {
+
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.id = id;
     this.read = read;
-
-    //test if new object can be directly added in bookShelf:
-    // bookLibrary.push(this);
+    this.id = id;
 }
 
-function addBook(book) {
+Book.prototype.readStatus = function() {
 
-
+    this.read = !this.read;
 }
 
-function removeBook(book) {
+function addBook(event) {
+
+    if (!form.reportValidity()) {
+        return;
+    }
+
+    event.preventDefault();
+
+    let bookTitle = document.querySelector("input[id='title']");
+    let bookAuthor = document.querySelector("input[id='author']");
+    let bookPages = document.querySelector("input[id='pages']");
+    let bookRead = document.querySelector("input[id='read']");
+    let bookId = crypto.randomUUID();
+
+    let book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked, bookId);
+    
+    bookLibrary.push(book);
 
 
+    showBook();
 }
 
-let newBook = new NewBook("Bible", "God", 1234, "0000", false);
-let newBook2 = new NewBook("asd", "fgh", 1111, "3333", true);
+function showBook() {
 
+    bookShelf.replaceChildren();
+    
+    for (let book of bookLibrary) {
+
+        // Create DOM elements
+
+        let bookElement = document.createElement("div");
+
+        let bookTitle = document.createElement("p");
+        let bookAuthor = document.createElement("p");
+        let bookPages = document.createElement("p");
+
+        let bookReadWrapper = document.createElement("div");
+        let bookReadStatus = document.createElement("input");
+        let bookRead = document.createElement("button");
+
+        let bookRemove = document.createElement("button");
+
+        // Assign element attributes
+
+        bookElement.classList.add("book", `data-${book.id}`);
+
+        bookTitle.classList.add("title");
+        bookAuthor.classList.add("author");
+        bookPages.classList.add("pages");
+
+        bookReadWrapper.classList.add("readWrapper");
+
+        bookReadStatus.setAttribute("type","checkbox");
+        bookReadStatus.setAttribute("disabled","");
+
+        bookRemove.classList.add("remove");
+
+
+        // Add corresponding element values
+
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = `written by ${book.author}`;
+        bookPages.textContent = `${book.pages} pages`;
+        bookReadStatus.checked = book.read;
+
+        if (bookReadStatus.checked) {
+            bookRead.textContent = "already read";
+        }
+        else {
+            bookRead.textContent = "not yet read";
+        }
+        
+        bookRemove.textContent = "Checkout Book";
+
+
+        // Append DOM elements
+
+        bookShelf.appendChild(bookElement);
+
+        bookElement.appendChild(bookTitle);
+        bookElement.appendChild(bookAuthor);
+        bookElement.appendChild(bookPages);
+
+        bookElement.appendChild(bookReadWrapper);
+        bookReadWrapper.appendChild(bookReadStatus);
+        bookReadWrapper.appendChild(bookRead);
+
+        bookElement.appendChild(bookRemove);
+        
+
+        bookRead.addEventListener("click",()=>{
+            
+            book.readStatus();
+            bookReadStatus.checked = book.read;
+
+            if (bookReadStatus.checked) {
+            bookRead.textContent = "already read";
+            }
+            else {
+                bookRead.textContent = "not yet read";
+            }
+        
+            
+            }
+        );
+
+        bookRemove.addEventListener("click", ()=>{
+            
+            bookShelf.removeChild(bookElement);
+
+            bookLibrary = bookLibrary.filter((item)=>{
+                if (item.id != book.id) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            });
+            
+            } 
+        );
+        
+    }
+
+    form.reset();
+    
+}
+
+
+
+let book1 = new Book("The Art of War",
+                    "Sun Tzu",
+                    "100",
+                    false,
+                    "0000-1111-2222"
+);
+
+let book2 = new Book("Meditations",
+                    "Marcus Aurelius",
+                    "200",
+                    true,
+                    "3333-4444-5555"
+);
+
+bookLibrary.push(book1, book2);
 
 let bookShelf = document.querySelector(".book-shelf");
-let book = document.createElement("div");
-
-// book.textContent = "test";
-
-for (item in newBook) {
-    book.textContent += newBook[item];
-}
-
-bookShelf.appendChild(book);
-
-console.log(bookLibrary);
+// let book = document.createElement("div");
 
 
 let form = document.querySelector('form');
 let showForm = document.querySelector('button.show');
+let submitForm = document.querySelector('button[type=submit]');
 
 showForm.addEventListener("click",()=>{
     // form.setAttribute("style","visibility:visible");
     form.classList.toggle("show");
     showForm.classList.toggle("hide");
 
-    console.log("Yo");
-}
-   
+    }
 );
 
-// showForm.textContent = "Test Button tho"
 
-// console.log(showFrom);
+submitForm.addEventListener("click", addBook);
+showBook();
